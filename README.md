32d4c94803574a69bc4d108893427e0c19c07141
ClearLove443 <1127280933@qq.com>
2021-10-21 22:51:24 +0800

    init fastapi

---
+++ README.md
@@ -0,0 +1,25 @@
+Ues fastapi framework
+
+# environment
+
+```bash
+python -m venv env
+source env/Scripts/activate
+```
+
+# installation
+
+```bash
+pip install autopep8
+pip install fastapi
+pip install "uvicorn[standard]"
+```
+
+# run it
+
+```bash
+uvicorn main:app --reload
+```
+
+# Interactive Api docs upgrad
+go to http://127.0.0.1:8000/docs
---
+++ api/api_v1/endpoints/demo.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+@router.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@router.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@router.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
